aa36960aa106ff9e5788e8b0cc88236bbed70e44
tanjinasubrin <112177526+tanjinasubrin@users.noreply.github.com>
2022-11-01 18:30:34 +0600

    Add files via upload

---
+++ tanjin_portfolio/urls.py
@@ -0,0 +1,10 @@
+
+from django.contrib import admin
+from django.urls import path
+from portfolio import views
+
+app_name = 'portfolio'
+
+urlpatterns = [
+    path('', views.portfolio, name='portfolio'),
+]
